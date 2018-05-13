30c1d2fc6d015176e073cba405410b3127d361bc
guillaume <g.a.levieux@gmail.com>
2018-05-13 10:53:47 +0200

    Better AI

--- Assets/_Completed-Assets/Scripts/Tank/TankAI.cs
+++ Assets/_Completed-Assets/Scripts/Tank/TankAI.cs
@@ -11,6 +11,7 @@ public class TankAI : MonoBehaviour {
     public float timeBetweenShot = 1.0f;
     public float speedTurn = 1.0f;
     public float speedMove = 0.5f;
+    public float precision = 0.5f;
     private float cooldownShot = 0;
 
 	// Use this for initialization
@@ -27,8 +28,20 @@ public class TankAI : MonoBehaviour {
 	
 	// Update is called once per frame
 	void LateUpdate () {
-        float distance = (player.position - transform.position).magnitude;
-        float angle = Vector3.SignedAngle(transform.forward, player.position - transform.position, Vector3.up);
+        //Calcul des params de shoot si le joueur ne bougeait pas
+        float distance = (transform.GetComponent<Complete.TankShooting>().m_FireTransform.position - player.position).magnitude;
+        float theta = Mathf.Asin(transform.GetComponent<Complete.TankShooting>().m_FireTransform.forward.y);
+        float g = Mathf.Abs(Physics.gravity.y);
+        float speed = Mathf.Sqrt((distance / (Mathf.Sin(2 * theta))) * g);
+        float time = (2 * speed * Mathf.Sin(theta)) / g;
+
+        //On prevoit la position de l'ennemi si ce temps s'écoule
+        Vector3 nextPos = player.transform.position + (player.GetComponent<Complete.TankMovement>().m_Speed *
+            player.GetComponent<Complete.TankMovement>().m_MovementInputValue * time * player.transform.forward);
+
+        //On se base sur la prochaine position
+        distance = (nextPos - transform.position).magnitude;
+        float angle = Vector3.SignedAngle(transform.forward, nextPos - transform.position, Vector3.up);
         tm.m_TurnInputValue = angle/50 * speedTurn;
         tm.m_MovementInputValue = speedMove;
 
@@ -38,7 +51,7 @@ public class TankAI : MonoBehaviour {
         bool tooClose = false;
         for(int i = 0; i < ennemies.Length; i++)
         {
-            float dist = (player.position - ennemies[i].position).magnitude;
+            float dist = (nextPos - ennemies[i].position).magnitude;
             if(ennemies[i]!= transform)
             {
                 if(dist < 15)
@@ -51,7 +64,11 @@ public class TankAI : MonoBehaviour {
 
         if (Mathf.Abs(angle) < 10 && cooldownShot <= 0 && !tooClose)
         {
-            ts.m_CurrentLaunchForce = Mathf.Max(15.0f,distance);
+            //On vise le poinit ou va se trouver le joueur
+            distance = (transform.GetComponent<Complete.TankShooting>().m_FireTransform.position - nextPos).magnitude;
+            speed = Mathf.Sqrt((distance / (Mathf.Sin(2 * theta))) * g);
+            
+            ts.m_CurrentLaunchForce = Mathf.Max(15.0f, speed);
             ts.Fire();
             cooldownShot = timeBetweenShot;
         }
