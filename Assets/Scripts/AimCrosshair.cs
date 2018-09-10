9cb7c3ed9481a02ea0ded0c63dd29522112f1139
guillaume levieux <g.a.levieux@gmail.com>
2018-09-10 11:34:37 +0200

    V2

---
+++ Assets/Scripts/AimCrosshair.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimCrosshair : MonoBehaviour {
+
+    public Transform Ground;
+    public Transform Crosshair;
+    
+    public Plane plane;
+
+	// Use this for initialization
+	void Start () {
+        plane = new Plane(Ground.position, Ground.up);
+        Cursor.visible = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        plane = new Plane(Ground.up,Ground.position);
+        // create a ray from the mousePosition
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        // plane.Raycast returns the distance from the ray start to the hit point
+        float distance = 100;
+       
+        if (plane.Raycast(ray, out distance))
+        {
+            Debug.Log("hit!!!");
+
+            // some point of the plane was hit - get its coordinates
+            Vector3 hitPoint = ray.GetPoint(distance);
+            
+            Crosshair.position = hitPoint + Ground.up;
+        }
+    }
+}
